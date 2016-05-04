b1a0b27544e1c26f268cd0d61fe010371cd91a95
user1 <jason.basanese@gmail.com>
2016-05-04 16:50:33 +0000

    removed some irrelevant code

--- hidden/levels/level10.php
+++
@@ -1,6 +0,0 @@
-<?php include('session.php'); ?>
-<p>More content to be added in the future. However if you would like to continue for whatever reason type more in the field below and click submit.</p>
-<form action="userhome.php" method="post">
-<input type="text" name="answer">
-<input type="submit" value="submit answer">
-</form>
--- hidden/levels/level11.php
+++
@@ -1,5 +0,0 @@
-<?php include('session.php'); ?>
-<p>You most sincerely are very bored. Go to bed.</p>
-<?php if ($_POST['answer'] == 'more'): ?>
-<?php $_SESSION['correct'] = 'true'; ?>
-<?php endif; ?>
